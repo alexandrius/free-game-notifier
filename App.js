6f8cc2b362dbcd6408723332268d341315550b95
Alexander Pataridze <s.pataridze@gmail.com>
2022-11-27 00:35:47 +0400

    refactor: update rn-iphone-helper improve statusbar android

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import 'react-native-url-polyfill/auto';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { fill } from 'styles/common';
 
 import Root from './src/root';
+
+if (Platform.OS === 'android') {
+  StatusBar.setTranslucent(true);
+  StatusBar.setBackgroundColor('transparent');
+}
 
 StatusBar.setBarStyle('light-content');
 
