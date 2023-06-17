d7d9a0c4ee3e5d525646e1da85c368a7937096ca
Alexander Pataridze <s.pataridze@gmail.com>
2023-06-17 18:02:36 +0400

    refactor: server

--- server/index.js
+++ server/index.js
@@ -7,13 +7,19 @@ console.log(mongoUrl);
 const client = new MongoClient(mongoUrl);
 await client.connect();
 const db = client.db("FreeStuff");
-const collection = db.collection("Games");
-const result = await collection.find();
-const array = await result.toArray();
+const gameCollection = db.collection("Games");
 
 const app = new Hono();
 app.get("/", (c) => c.text("Hello Bun!"));
-app.get("/api/games", (c) => c.json(array));
+app.get("/api/games", async (c) => {
+  const result = await gameCollection.find();
+  const array = await result.toArray();
+
+  return c.json(array);
+});
+app.get("/api/games/:id", (c) => {
+  console.log(c.req.param("id"));
+});
 
 export default {
   port: 3000,
