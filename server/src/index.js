6a2b48181a27c4c6879fa2c7f3214987db09ea1e
Alexander Pataridze <s.pataridze@gmail.com>
2023-06-18 18:33:41 +0400

    temp: commit nonsense

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,24 @@
 import { MongoClient } from "mongodb";
 import { Hono } from "hono";
-import { DB_URI } from "env";
+// import { DB_URI } from "env";
 
-const mongoUrl = DB_URI;
-
-console.log(mongoUrl);
-const client = new MongoClient(mongoUrl);
-await client.connect();
-const db = client.db("FreeGameNotifier");
-const gameCollection = db.collection("Games");
+// const mongoUrl = DB_URI;
+// const client = new MongoClient(mongoUrl);
+// await client.connect();
+// const db = client.db("FreeGameNotifier");
+// const gameCollection = db.collection("Games");
 
 const app = new Hono();
 app.get("/", (c) => c.text("Hello Bun!"));
-app.get("/api/games", async (c) => {
-  const result = await gameCollection.find();
-  const array = await result.toArray();
 
-  return c.json(array);
+app.get("/api/games", async (c) => {
+  return c.json({});
+  // const result = await gameCollection.find();
+  // const array = await result.toArray();
+  // return c.json(array);
 });
 app.get("/api/games/:id", (c) => {
-  console.log(c.req.param("id"));
+  // console.log(c.req.param("id"));
 });
 
 export default {
