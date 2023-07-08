f43303c624f537bf87a926a27777639991c3a55b
Alexander Pataridze <s.pataridze@gmail.com>
2023-07-08 21:15:58 +0400

    chore: remove files

---
+++ manager/app/_layout.js
@@ -0,0 +1,13 @@
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  return <Stack></Stack>;
+}
